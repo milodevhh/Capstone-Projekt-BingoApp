f93db5f08487cf9235ff800cbdb8428c152117be
milodevhh <schmiesseklgmail.com>
2023-08-14 16:45:57 +0200

    add a onclick function - change color #F2E7AE - toggle

--- components/Card.js
+++ components/Card.js
@@ -1,14 +1,17 @@
 import cards from "@/lib/db";
-import { Fragment } from "react";
+import { Fragment, useState } from "react";
 import styled from "styled-components";
 
 export default function Card({}) {
-  console.log(cards);
+  const [backgroundColor, setBackgroundColor] = useState("#add1d9");
+  function handleClick() {
+    setBackgroundColor(backgroundColor === "#add1d9" ? "#F2E7AE" : "#add1d9");
+  }
   return (
     <GridContainer>
       {cards.map((card) => (
         <Fragment key={card.id}>
-          <GameCards>
+          <GameCards onClick={handleClick} style={{ backgroundColor }}>
             {card.name} {card.icon}
           </GameCards>
         </Fragment>
@@ -21,11 +24,13 @@ const GridContainer = styled.div`
   display: grid;
   grid-template-columns: repeat(3, 1fr);
   grid-template-rows: repeat(3, 1fr);
-  grid-column-gap: 0px;
-  grid-row-gap: 0px;
+  grid-column-gap: 0.5rem;
+  grid-row-gap: 0.5rem;
+  padding: 2rem;
 `;
 
 const GameCards = styled.button`
+  height: 5rem;
   background-color: #add1d9;
   border-radius: 1rem;
   border: solid black 0.1rem;
