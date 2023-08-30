1427706cd1c692e5273036b815a92c500777a67e
milodevhh <schmiesseklgmail.com>
2023-08-30 12:24:22 +0200

    add const bingo add styling

--- pages/index.js
+++ pages/index.js
@@ -2,26 +2,63 @@ import GridOfCards from "@/components/GridOfCards";
 import Navigation from "@/components/Navigation";
 import { WinIcon } from "@/components/SVG/win";
 import { StyledButton } from "@/components/StyledButton";
+import { styled } from "styled-components";
 
 export default function HomePage({
   handleActiveCards,
   shuffledCards,
   shuffle,
 }) {
+  const firstHorizontalRowWin =
+    shuffledCards[0]?.isActive &&
+    shuffledCards[1]?.isActive &&
+    shuffledCards[2]?.isActive;
+  const secondHorizontalRowWin =
+    shuffledCards[3]?.isActive &&
+    shuffledCards[4]?.isActive &&
+    shuffledCards[5]?.isActive;
+  const thirdHorizontalRowWin =
+    shuffledCards[6]?.isActive &&
+    shuffledCards[7]?.isActive &&
+    shuffledCards[8]?.isActive;
+  const firstHorizontalColumWin =
+    shuffledCards[0]?.isActive &&
+    shuffledCards[3]?.isActive &&
+    shuffledCards[6]?.isActive;
+  const secondHorizontalColumWin =
+    shuffledCards[1]?.isActive &&
+    shuffledCards[4]?.isActive &&
+    shuffledCards[7]?.isActive;
+  const thirdHorizontalColumWin =
+    shuffledCards[2]?.isActive &&
+    shuffledCards[5]?.isActive &&
+    shuffledCards[8]?.isActive;
+  const bingo =
+    firstHorizontalRowWin ||
+    secondHorizontalRowWin ||
+    thirdHorizontalRowWin ||
+    firstHorizontalColumWin ||
+    secondHorizontalColumWin ||
+    thirdHorizontalColumWin;
+
   return (
     <main>
-      <h1>Bingo</h1>
+      <h1>Road Trip Bingo</h1>
       <h2>Play</h2>
       <p>
-        Click on a card if you have seen what is shown on the card in your
+        Click on a card if you have seen what is shown on it in your
         surroundings. Have fun playing...
       </p>
       <GridOfCards
         cards={shuffledCards}
         handleActiveCards={handleActiveCards}
       />
-      <StyledButton onClick={shuffle}>Create new Bingo</StyledButton>
-      <WinIcon color="" width="3rem" height="3rem" />
+      <StyledButton onClick={shuffle}>
+        {bingo ? "Restart the game" : "Create new Bingo"}
+      </StyledButton>
+
+      {bingo ? <WinIcon color="" width="3rem" height="3rem" /> : null}
+      {bingo ? <StyledWinText>Congratulations you won!</StyledWinText> : null}
 
       <p>
         The first player to complete a horizontal or vertical line shouts bingo
@@ -31,3 +68,7 @@ export default function HomePage({
     </main>
   );
 }
+
+const StyledWinText = styled.p`
+  color: #f2b988;
+`;
