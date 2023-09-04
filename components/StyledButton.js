f33ae6b6b3f59fd94defca9289bc4e2309e3593b
milodevhh <schmiesseklgmail.com>
2023-09-04 11:19:28 +0200

    fixed Button Component

--- components/StyledButton.js
+++ components/StyledButton.js
@@ -1,6 +1,10 @@
 import { styled } from "styled-components";
+import { Sono } from "@next/font/google";
+
+const sono = Sono({ subsets: ["latin"] });
 
 export const StyledButton = styled.button`
+  font-family: ${sono.style.fontFamily};
   padding: 0.8rem;
   border-radius: 0.6rem;
   color: black;
